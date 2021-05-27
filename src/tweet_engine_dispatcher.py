88e4e99b828774bdd7a449133170bd1783e39d6f
Sam <sam@tealstreet.io>
2021-05-27 02:27:07 -0700

    combined streams into 1

---
+++ src/tweet_engine_dispatcher.py
@@ -0,0 +1,14 @@
+from constants import ELON_TWITTER_ACCOUNT_ID, DELTAONE_TWITTER_ACCOUNT_ID, FIRSTSQUAWK_TWITTER_ACCOUNT_ID, \
+    WHALEALERT_TWITTER_ACCOUNT_ID
+from tweet_engine_elon import tweet_engine_elon
+from tweet_engine_news import tweet_engine_news
+from tweet_engine_whalealert import tweet_engine_whalealert_helper
+
+
+def tweet_engine_dispatcher(status):
+    if status.user.id_str == ELON_TWITTER_ACCOUNT_ID:
+        tweet_engine_elon(status)
+    elif status.user.id_str in [DELTAONE_TWITTER_ACCOUNT_ID, FIRSTSQUAWK_TWITTER_ACCOUNT_ID]:
+        tweet_engine_news(status)
+    elif status.user.id_str == WHALEALERT_TWITTER_ACCOUNT_ID:
+        tweet_engine_whalealert_helper(status)
