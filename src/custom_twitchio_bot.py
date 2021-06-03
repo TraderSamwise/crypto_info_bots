2c26c90bea03f0505e66c1044f269e3bfd24a448
Sam <sam@tealstreet.io>
2021-06-02 19:23:44 -0700

    fixed position fetching on ftx

---
+++ src/custom_twitchio_bot.py
@@ -0,0 +1,89 @@
+from twitchio.ext.commands import CheckFailure
+from twitchio.ext.commands.stringparser import StringParser
+from twitchio.ext import commands
+
+class CustomTwitchBot(commands.Bot):
+
+    async def event_message(self, message):
+        # print(message.content)
+        # await self.send_message_direct("dude")
+        # print(message)
+        try:
+            await self.handle_commands(message)
+        except Exception as e:
+            print(e)
+
+    async def handle_commands(self, message, ctx=None):
+        if ctx is None:
+            try:
+                ctx = await self.get_context(message)
+            except Exception as e:
+                return await self.event_error(e, message.raw_data)
+
+        if not ctx.prefix:
+            return
+
+        content = message.content
+        content = content[len(ctx.prefix)::].lstrip(' ')
+        parsed = StringParser().process_string(content)
+
+        message.clean_content = ' '.join(parsed.values())
+
+        try:
+            command = parsed.pop(0)
+        except KeyError:
+            return
+
+        try:
+            command = self._aliases[command]
+        except KeyError:
+            pass
+
+        try:
+            if command in self.commands:
+                command = self.commands[command]
+            elif command:
+                return
+        except Exception as e:
+            ctx.command = None
+            return await self.event_command_error(ctx, e)
+
+        ctx.command = command
+        instance = ctx.command.instance
+
+        try:
+            result = await self._handle_checks(ctx, command.no_global_checks)
+        except Exception as e:
+            return await self.event_command_error(ctx, e)
+        else:
+            if callable(result):
+                return await self.event_command_error(ctx, CheckFailure(f'The command <{command.name}> failed to invoke'
+                                                                        f' due to checks:: {result.__name__}'))
+            elif not result:
+                raise CheckFailure(f'The command <{command.name}> failed to invoke due to checks.')
+
+        try:
+            ctx.args, ctx.kwargs = await command.parse_args(instance, parsed)
+
+            await self.global_before_hook(ctx)
+
+            if ctx.command._before_invoke:
+                await ctx.command._before_invoke(instance, ctx)
+
+            if instance:
+                await ctx.command._callback(instance, ctx, *ctx.args, **ctx.kwargs)
+            else:
+                await ctx.command._callback(ctx, *ctx.args, **ctx.kwargs)
+        except Exception as e:
+            if ctx.command.on_error:
+                await ctx.command.on_error(instance, ctx, e)
+
+            await self.event_command_error(ctx, e)
+
+        try:
+            # Invoke our after command hooks...
+            if command._after_invoke:
+                await ctx.command._after_invoke(ctx)
+            await self.global_after_hook(ctx)
+        except Exception as e:
+            await self.event_command_error(ctx, e)
--- src/sender_twitch.py
+++ src/sender_twitch.py
@@ -5,8 +5,8 @@ from threading import Thread
 
 import ccxt
 from twitchio.ext import commands
-from twitchio.ext.commands import CheckFailure
-from twitchio.ext.commands.stringparser import StringParser
+
+from custom_twitchio_bot import CustomTwitchBot
 
 bot_account_name = "BOT_ACCOUNT_NAME"
 token = os.getenv("TWITCH_OAUTH_TOKEN")
@@ -27,7 +27,7 @@ exchange_ftx = ccxt.ftx({
 exchange_ftx.load_markets()
 
 def format_usd(v):
-    return round(float(v), 2)
+    return "{:,}".format(round(float(v), 2))
 
 def format_btc(v):
     return round(float(v), 4)
@@ -45,19 +45,20 @@ def format_bybit_positions(positions):
         msgs.append(f'[symbol: {pos["symbol"]}, side: {format_side(pos["side"])}, size: {format_usd(pos["size"])} USD, entry: {format_usd(pos["entry_price"])}, UnrlPNL: {format_btc(pos["unrealised_pnl"])}]')
     return msgs
 
+def format_ftx_size(v):
+    return format_usd(v[1:])
 
 def format_ftx_positions(positions):
     msgs = []
     for pos in positions:
-        msgs.append(f'[symbol: {pos["future"]}, side: {format_side(pos["side"])}, size: {pos["size"]}, entry: {pos["entryPrice"]}]')
+        msgs.append(f'[symbol: {pos["future"]}, side: {format_side(pos["side"])}, size: {format_ftx_size(pos["cost"])} USD, entry: {format_usd(pos["recentAverageOpenPrice"])}, B/E: {format_usd(pos["recentBreakEvenPrice"])}, PNL: {format_usd(pos["recentPnl"])} USD]')
     return msgs
 
-class Bot(commands.Bot):
+class Bot(CustomTwitchBot):
 
     def __init__(self):
         super().__init__(irc_token=token, client_id=client_id, nick='botbotsamwise', prefix='!',
                          initial_channels=['tradersamwise'])
-
 
     # Events don't need decorators when subclassed
     async def event_ready(self):
@@ -65,103 +66,19 @@ class Bot(commands.Bot):
         self.chan = bot.get_channel("tradersamwise")
         print(self.chan)
 
-    async def event_message(self, message):
-        # print(message.content)
-        # await self.send_message_direct("dude")
-        # print(message)
-        try:
-            await self.handle_commands(message)
-        except Exception as e:
-            print(e)
-
-    async def handle_commands(self, message, ctx=None):
-        if ctx is None:
-            try:
-                ctx = await self.get_context(message)
-            except Exception as e:
-                return await self.event_error(e, message.raw_data)
-
-        if not ctx.prefix:
-            return
-
-        content = message.content
-        content = content[len(ctx.prefix)::].lstrip(' ')
-        parsed = StringParser().process_string(content)
-
-        message.clean_content = ' '.join(parsed.values())
-
-        try:
-            command = parsed.pop(0)
-        except KeyError:
-            return
-
-        try:
-            command = self._aliases[command]
-        except KeyError:
-            pass
-
-        try:
-            if command in self.commands:
-                command = self.commands[command]
-            elif command:
-                return
-        except Exception as e:
-            ctx.command = None
-            return await self.event_command_error(ctx, e)
-
-        ctx.command = command
-        instance = ctx.command.instance
-
-        try:
-            result = await self._handle_checks(ctx, command.no_global_checks)
-        except Exception as e:
-            return await self.event_command_error(ctx, e)
-        else:
-            if callable(result):
-                return await self.event_command_error(ctx, CheckFailure(f'The command <{command.name}> failed to invoke'
-                                                                        f' due to checks:: {result.__name__}'))
-            elif not result:
-                raise CheckFailure(f'The command <{command.name}> failed to invoke due to checks.')
-
-        try:
-            ctx.args, ctx.kwargs = await command.parse_args(instance, parsed)
-
-            await self.global_before_hook(ctx)
-
-            if ctx.command._before_invoke:
-                await ctx.command._before_invoke(instance, ctx)
-
-            if instance:
-                await ctx.command._callback(instance, ctx, *ctx.args, **ctx.kwargs)
-            else:
-                await ctx.command._callback(ctx, *ctx.args, **ctx.kwargs)
-        except Exception as e:
-            if ctx.command.on_error:
-                await ctx.command.on_error(instance, ctx, e)
-
-            await self.event_command_error(ctx, e)
-
-        try:
-            # Invoke our after command hooks...
-            if command._after_invoke:
-                await ctx.command._after_invoke(ctx)
-            await self.global_after_hook(ctx)
-        except Exception as e:
-            await self.event_command_error(ctx, e)
-
 
     async def positions_helper(self, ctx):
         if not hasattr(self, "_last_fetch_positions"):
             self._last_fetch_positions = datetime.datetime(2009, 1, 6, 15, 8, 24, 78915)
         now = datetime.datetime.now()
-        if (now - self._last_fetch_positions).seconds < 15:
+        if (now - self._last_fetch_positions).seconds < 5:
             return
         self._last_fetch_positions = now
         bybit_positions = exchange_bybit.v2_private_get_position_list()
         bybit_open_positions = [position['data'] for position in bybit_positions['result'] if position['data']['size'] != '0']
-        ftx_positions = exchange_ftx.fetch_positions()
-        ftx_open_positions = [position for position in ftx_positions if position['size'] != '0.0']
-        # msg = f'Bybit: {ctx.author.name}!'
+        # https://github.com/ccxt/ccxt/issues/9213
+        ftx_positions = exchange_ftx.private_get_positions({'showAvgPrice': False})
+        ftx_open_positions = [position for position in ftx_positions['result'] if position['size'] != '0.0']
         bybit_msgs = format_bybit_positions(bybit_open_positions)
         for msg in bybit_msgs:
             await ctx.send("ByBit: " + msg)
